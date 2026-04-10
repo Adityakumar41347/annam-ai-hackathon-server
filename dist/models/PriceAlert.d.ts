764a50fb8439e25c8c1955e91024a567a7fedb28
Adityakumar41347 <adityakumar41347@gmail.com>
2026-04-10 14:24:17 +0530

    Delete dist directory

--- dist/models/PriceAlert.d.ts
+++
@@ -1,8 +0,0 @@
-import type { IPriceAlert } from "../types";
-export declare const PriceAlert: import("mongoose").Model<IPriceAlert, {}, {}, {}, import("mongoose").Document<unknown, {}, IPriceAlert, {}, {}> & IPriceAlert & Required<{
-    _id: import("mongoose").Types.ObjectId;
-}> & {
-    __v: number;
-}, any>;
-export default PriceAlert;
-//# sourceMappingURL=PriceAlert.d.ts.map
